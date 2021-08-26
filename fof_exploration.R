c620ce34f6c6dfeac99c4884fcb035efede78939
Kenny Yang <kennyyang19@gmail.com>
2021-08-26 19:35:12 -0400

    initial committ

---
+++ fof_exploration.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+latest_data <-
+read_csv("https://raw.githubusercontent.com/estelleou/fofdata/main/all_sectors_levels_q.csv") %>% 
+
+series_code <- 
+read_delim("https://raw.githubusercontent.com/estelleou/fofdata/main/all_sectors_levels_q.txt ")
